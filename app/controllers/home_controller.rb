37c7cc4612352fee3b0e25f7f4b1e95c64b4f2ba
snsubbu <subbu@paramountconsultancyservices.com>
2013-06-08 18:48:55 +0530

    home updates

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,9 @@ class HomeController < ApplicationController
   before_filter :authenticate_user!
 
   def index
-    @posts = Post.all
+   @posts = Post.all
+    @posts = Post.order('created_at desc').limit(5)
+
     @technologies = Technology.all
     @user = User.all
   end
